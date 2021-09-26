62c69bbcafa2b87170d455c50a8a79632f600fcb
Martin Jindra <mjindra@derchef.email>
2021-09-26 22:10:39 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 # nginx configuration
 mkdir -p /opt/docker-files/nginx-proxy/
 echo "allow all;" >> /opt/docker-files/nginx-proxy/network_internal.conf
-echo "client_max_body_size 10G;" >> /opt/docker-files/nginx-proxy/uploadsize.conf
+echo "client_max_body_size 50G;" >> /opt/docker-files/nginx-proxy/uploadsize.conf
